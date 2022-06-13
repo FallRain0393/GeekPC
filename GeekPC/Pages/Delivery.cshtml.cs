7c58fd764cbc8658fb6c1eda045aefb30dc9e460
auto-meh2.ru@outlook.com <auto-meh2.ru@outlook.com>
2022-06-13 03:27:05 +0300

    13.06.2022

---
+++ GeekPC/Pages/Delivery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeekPC.Pages
+{
+    public class DeliveryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
